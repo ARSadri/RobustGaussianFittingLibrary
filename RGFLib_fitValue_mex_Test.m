ae6e6e828c6b710cd91712681eb9e7a2c719393c
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-06-11 11:42:58 +0200

    Update RGFLib_fitValue_mex_Test.m

--- RGFLib_fitValue_mex_Test.m
+++ RGFLib_fitValue_mex_Test.m
@@ -1,6 +1,28 @@
 clear
-mex -g RGFLib_fitValue_mex.c
 
-inVec = [randn(1, 1000) 10*rand(1,300)]
+%uncomment for MEX compile and then comment again for MATLAB use
+%mex -g RGFLib_fitValue_mex.c, return
 
-[Rmean, Rstd] = RGFLib_fitValue_mex(inVec, median(inVec), numel(inVec), 0.5, 0.4, 3.0, 12)
+
+N = 1000
+topKthPerc = 0.5
+bottomKthPerc = 0.45
+MSSE_LAMBDA = 3.0
+optIters = 12
+
+Gaus_mean = 43232
+Gaus_std = 1912
+uniform_spread = 1232312
+uniform_bias = 75432
+initModel = 87332111
+
+inliers = Gaus_mean + Gaus_std*randn(1, N*topKthPerc);
+outliers = uniform_spread*(rand(1,N*(1-topKthPerc))-0.5)+uniform_bias;
+outliers(abs(outliers-Gaus_mean)<Gaus_std*MSSE_LAMBDA)=[];
+inVec = [inliers outliers];
+N = numel(inVec)
+
+%maybe you would like to help the optimization a bit if you think median is an inlier??? no?
+%initModel = median(inVec)
+
+[Rmean, Rstd] = RGFLib_fitValue_mex(inVec, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters)
