37fd786903824b5228c49c21b4eeaeda8c8d4b5e
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-06-11 11:15:39 +0200

    Update RGFLib_fitValue_mex_Test.m

--- RGFLib_fitValue_mex_Test.m
+++ RGFLib_fitValue_mex_Test.m
@@ -1,18 +1,6 @@
-clc
 clear
-close all
 mex -g RGFLib_fitValue_mex.c
 
-inVec = randn(1, 1000);
-inVec = [inVec 10*(rand(1,300)+0.3)]
+inVec = [randn(1, 1000) 10*rand(1,300)]
 
-[Rmean, Rstd] = RGFLib_fitValue_mex(inVec, median(inVec), numel(inVec), 0.5, 0.4, 3.0, 100)
-
-return
-[bins, edges] = hist(inVec, 40)
-
-[~, indMax] = min(abs(edges-Rmean))
-
-plot(edges, bins)
-hold on
-plot(edges, bins(indMax)*exp(((edges-Rmean).^2)./(2*Rstd.^2)),'r')
+[Rmean, Rstd] = RGFLib_fitValue_mex(inVec, median(inVec), numel(inVec), 0.5, 0.4, 3.0, 12)
