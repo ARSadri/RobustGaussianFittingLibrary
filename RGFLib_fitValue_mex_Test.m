4ca8eee2c5c8abf534c74c7a63c586f907ca1b2c
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-06-16 23:06:02 +0200

    Update RGFLib_fitValue_mex_Test.m

--- RGFLib_fitValue_mex_Test.m
+++ RGFLib_fitValue_mex_Test.m
@@ -10,22 +10,23 @@ bottomKthPerc = 0.45
 MSSE_LAMBDA = 3.0	%std away from mean is still a guassians.
 optIters = 12
 
-Gaus_mean = 0
+Gaus_mean = 100
 Gaus_std = 1
 uniform_spread = 10
 uniform_bias = 0
 initModel = 10
 
-inliers = Gaus_mean + Gaus_std*randn(1, N*topKthPerc);
-outliers = uniform_spread*(rand(1,N*(1-topKthPerc))-0.5)+uniform_bias;
+inliers = Gaus_mean + Gaus_std*randn(1, N*0.6);
+inliers_W = rand(1, length(inliers));
+outliers = uniform_spread*(rand(1,N*(1-0.6))-0.5)+uniform_bias;
 outliers(abs(outliers-Gaus_mean)<Gaus_std*MSSE_LAMBDA)=[];
+outliers_W = 10*rand(1, length(outliers));
 inVec = [inliers outliers];
 N = numel(inVec)
 
 %maybe you would like to help the optimization a bit if you think median is an inlier??? no?
 %initModel = median(inVec)
 
-inWeights = rand(1,N)
-inWeights = inWeights/sum(inWeights) 
+inWeights = [inliers_W outliers_W];
 
-[Rmean, Rstd] = RGFLib_fitValue_mex(inVec, inWeights, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters)
+Rmean = RGFLib_fitValue_mex(inVec, inWeights, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters)
