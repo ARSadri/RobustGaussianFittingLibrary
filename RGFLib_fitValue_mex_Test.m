d229c7cee5c8df8860350beebc121e3ae374520b
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-06-17 22:55:46 +0200

    Update RGFLib_fitValue_mex_Test.m

--- RGFLib_fitValue_mex_Test.m
+++ RGFLib_fitValue_mex_Test.m
@@ -1,22 +1,23 @@
 %uncomment for MEX compile and then comment again for MATLAB use
 %mex RGFLib_fitValue_mex.c, return
-close all
+
 numTests = 1000;
 Rmean_rec=zeros(1,numTests);
 for test = 1: numTests
-	N = 10;
-	inlierPerc = 0.75;
+	N = 30;
+	inlierPerc = 0.51;
 	topKthPerc = 0.5;
 	bottomKthPerc = 0.4;
 	MSSE_LAMBDA = 3.0;	%std away from mean is still a guassians.
 	optIters = 12;
-
+	skewFlag = 1;
+	
 	Gaus_mean = 0;
 	Gaus_std = 1;
-	uniform_spread = 100;
-	uniform_bias = 0;
+	uniform_spread = 10000000;
+	uniform_bias = 10;
 	initModel = 0;
-
+	
 	inliers = Gaus_mean + Gaus_std*randn(1, floor(N*inlierPerc));
 	inliers_W = rand(1, length(inliers));
 	outliers = uniform_spread*(rand(1,floor(N*(1-inlierPerc)))-0.5)+uniform_bias;
@@ -33,13 +34,7 @@ for test = 1: numTests
 	%maybe you would like to help the optimization a bit if you think median is an inlier??? no?
 	%initModel = median(inVec)
 
-	Rmean = RGFLib_fitValue_mex(inVec, inWeights, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters);
+	Rmean = RGFLib_fitValue_mex(inVec, inWeights, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters, skewFlag);
 	Rmean_rec(test) = Rmean;
 end
 disp(median(Rmean_rec))
-return
-hist(inVec, 20)
-hold on
-plot([Rmode, Rmode], [0, floor(N/10)], 'green')
-plot([Rmean, Rmean], [0, floor(N/10)], 'red')
-
