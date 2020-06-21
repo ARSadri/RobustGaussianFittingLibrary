816f21610d26f8432488613965661d25b3808586
ARSadri <ARSadri@gmail.com>
2020-06-21 09:50:19 +0200

    Renaming the MATLAB support modules

---
+++ RGFLib_mex_fitValue2Skewed.c
@@ -0,0 +1,109 @@
+/*
+A Robust Gaussian Fitting Library
+	for ourlier detection and background subtraction
+
+This is a MEX C file for MATLAB as the
+	RGFLib.c external interface
+
+If you'd like to use it, compile it throught the test file.	
+	
+An example is made in RGFLibTest.m
+
+Currently we will write a mex for the following function(s):
+
+		void RobustSingleGaussianVec(float *vec, 
+					float *modelParams, float theta, 
+					unsigned int N,
+					float topKthPerc, float bottomKthPerc, 
+					float MSSE_LAMBDA, unsigned char optIters)
+
+Written by Alireza Sadri, arsadri@gmail.com 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "RGFLib.c"
+
+//////////////////////////// MEX ///////////////////////////
+#include <matrix.h>
+#include <mex.h>   
+#ifndef MWSIZE_MAX
+	typedef int mwSize;
+	typedef int mwIndex;
+	typedef int mwSignedIndex;
+	#define MWSIZE_MAX    2147483647UL
+	#define MWINDEX_MAX   2147483647UL
+	#define MWSINDEX_MAX  2147483647L
+	#define MWSINDEX_MIN -2147483647L
+	#define MWSIZE_MIN    0UL
+	#define MWINDEX_MIN   0UL
+#endif
+/////////////////////////////// END OF MEX ////////////////////////
+
+/* The gateway function */
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[]) {
+	unsigned int i;
+	double *inVec;
+	double *inWeights;
+	double *initModel;
+	double *N;
+	double *topKthPerc;
+	double *bottomKthPerc;
+	double *MSSE_LAMBDA;
+	double *optIters;
+	double *giveRStd;
+
+	//associate inputs
+	inVec 			= mxGetPr(prhs[0]);
+	inWeights	    = mxGetPr(prhs[1]);
+	initModel		= mxGetPr(prhs[2]);
+	N				= mxGetPr(prhs[3]);
+	topKthPerc		= mxGetPr(prhs[4]);
+	bottomKthPerc	= mxGetPr(prhs[5]);
+	MSSE_LAMBDA		= mxGetPr(prhs[6]);
+	optIters		= mxGetPr(prhs[7]);
+	giveRStd		= mxGetPr(prhs[8]);
+	
+	float *_inVec;
+	float *_inWeights;
+	float _initModel;
+	unsigned int _N;
+	float _topKthPerc;
+	float _bottomKthPerc;
+	float _MSSE_LAMBDA;
+	unsigned char _optIters;
+		
+	_initModel = (float)(initModel[0]);
+	_N = (unsigned int)(N[0]);
+	_topKthPerc = (float)(topKthPerc[0]);
+	_bottomKthPerc = (float)(bottomKthPerc[0]);
+	_MSSE_LAMBDA = (float)(MSSE_LAMBDA[0]);
+	_optIters = (unsigned char)(optIters[0]);
+	
+	_inVec=(float *) malloc( _N*sizeof(float));
+	for(i=0; i<_N; i++)
+		_inVec[i] = (float)inVec[i];
+
+	_inWeights=(float *) malloc( _N*sizeof(float));
+	for(i=0; i<_N; i++)
+		_inWeights[i] = (float)inWeights[i];
+
+	float _mP[2];
+	_mP[0] = 0;
+	_mP[1] = 0;
+	
+	fitValue2Skewed(_inVec, _inWeights, _mP, _initModel, _N,
+					   _topKthPerc, _bottomKthPerc,
+					   _MSSE_LAMBDA, _optIters);
+	free(_inVec);
+
+	plhs[0] = mxCreateDoubleScalar(1);
+	*mxGetPr(plhs[0]) = _mP[0];
+	if(giveRStd) {
+		plhs[1] = mxCreateDoubleScalar(1);
+		*mxGetPr(plhs[1]) = _mP[1];
+	}	
+	return;
+}
---
+++ RGFLib_mex_fitValue2Skewed_Test.m
@@ -0,0 +1,40 @@
+%uncomment for MEX compile and then comment again for MATLAB use
+mex RGFLib_mex_fitValue2Skewed.c, return
+
+numTests = 1000;
+Rmean_rec=zeros(1,numTests);
+for test = 1: numTests
+	N = 30;
+	inlierPerc = 0.51;
+	topKthPerc = 0.5;
+	bottomKthPerc = 0.4;
+	MSSE_LAMBDA = 3.0;	%std away from mean is still a guassians.
+	optIters = 12;
+	giveRStd = 1;
+	
+	Gaus_mean = 0;
+	Gaus_std = 1;
+	uniform_spread = 10000000;
+	uniform_bias = 10;
+	initModel = 0;
+	
+	inliers = Gaus_mean + Gaus_std*randn(1, floor(N*inlierPerc));
+	inliers_W = rand(1, length(inliers));
+	outliers = uniform_spread*(rand(1,floor(N*(1-inlierPerc)))-0.5)+uniform_bias;
+	%outliers(abs(outliers-Gaus_mean)<Gaus_std*MSSE_LAMBDA)=[];
+	outliers_W = 10*rand(1, length(outliers));
+	inVec = [inliers outliers];
+	inWeights = [inliers_W outliers_W];
+	N = numel(inVec);
+	if N<3
+		inVec = [inVec 1230];
+		inWeights = [inWeights 1];
+	end
+	N = numel(inVec);
+	%maybe you would like to help the optimization a bit if you think median is an inlier??? no?
+	%initModel = median(inVec)
+
+	Rmean, Rstd = RGFLib_mex_fitValue2Skewed(inVec, inWeights, initModel, N, topKthPerc, bottomKthPerc, MSSE_LAMBDA, optIters, giveRStd);
+	Rmean_rec(test) = Rmean;
+end
+disp(median(Rmean_rec))
