1d189e9a23aeaf2c474ee666663380cf532236ff
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-08-03 22:40:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup, Extension
 setup(
   name = 'RobustGaussianFittingLibrary',         # How you named your package folder (MyLib)
   packages = ['RobustGaussianFittingLibrary'],   # Chose the same as "name"
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library for robust Gaussian fitting using geometric models in presence of outliers. ',   # Give a short description about your library
   author = 'Alireza Sadri',                   # Type in your name
   author_email = 'ARSadri@domain.com',      # Type in your E-Mail
   url = 'https://github.com/ARSadri/RobustGaussianFittingLibrary',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ARSadri/RobustGaussianFittingLibrary/archive/v0.1.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ARSadri/RobustGaussianFittingLibrary/archive/v0.1.3.tar.gz',    # I explain this later on
   keywords = ['outlier', 'outlier detection', 'outlier removal', 'anamoly detection', 'curve fitting', 'line fitting', 'plane fitting', 'fit a Gaussian', 'Gaussian fitting'],   # Keywords that define your package best
   install_requires=[ 
           'numpy',
@@ -17,7 +17,7 @@ setup(
       ],
   #cmdclass={'install': 'make all'},
   ext_modules=[Extension(name = 'RGFLib', 
-                         sources = ['RGFLib.c'],
+                         sources = ['RobustGaussianFittingLibrary/RGFLib.c'],
                          language = 'c',
                          extra_compile_args = ['-shared'])],
   classifiers=[
