dc137d0944c34fc267c78a3a91510dc92af2e928
ARSadri <51805937+ARSadri@users.noreply.github.com>
2020-08-04 09:50:21 +0200

    Update tests.py

--- RobustGaussianFittingLibrary/tests.py
+++ RobustGaussianFittingLibrary/tests.py
@@ -1,3 +1,8 @@
+#################################################################################################
+# This file is part of RobustGaussianFittingLibrary, a free library WITHOUT ANY WARRANTY        # 
+# Copyright: 2019-2020 Deutsches Elektronen-Synchrotron                                         # 
+#################################################################################################
+
 import RobustGaussianFittingLibrary 
 import RobustGaussianFittingLibrary.useMultiproc
 import RobustGaussianFittingLibrary.misc
